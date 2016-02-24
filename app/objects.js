4941d6ec7af063fb01beea32a8b9e8ffaa84244d
Carlos Brito Lage <carlos@carloslage.net>
2016-02-24 16:51:47 -0500

    objects done

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,20 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+	return fn.apply(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+  	constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
-
+  	var result = [];
+  	for (var x in obj) {
+  		if (obj.hasOwnProperty(x)) {
+  			result.push(x + ': ' + obj[x]);
+  		}
+  	}
+  	return result;
   }
 };
